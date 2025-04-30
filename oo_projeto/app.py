83ca6576fca168491620d0863e70b034dcad83f1
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-04-30 10:17:28 -0300

    Gerenciando Avaliacoes

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -4,7 +4,10 @@ biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
 biblioteca_cidade.alterna_estado()
-biblioteca_shopping.alterna_estado()
+
+biblioteca_cidade.receber_avaliacao('Fulano', 8.5)
+biblioteca_cidade.receber_avaliacao('Ciclano', 9.5)
+biblioteca_cidade.receber_avaliacao('Fulano', 7.0)
 
 
 def main():
--- oo_projeto/modelos/biblioteca.py
+++ oo_projeto/modelos/biblioteca.py
@@ -1,9 +1,13 @@
+from modelos.avaliacao import Avaliacao
+
+
 class Biblioteca:
     bibliotecas = []
 
     def __init__(self, nome):
         self.nome = nome
         self._ativo = False
+        self._avaliacao = []
         Biblioteca.bibliotecas.append(self)
 
     def __str__(self):
@@ -11,9 +15,11 @@ class Biblioteca:
 
     @classmethod
     def listar_bibliotecas(cls):
-        print(f"{'Nome da biblioteca'.ljust(25)} | {'Status'}")
+        print(
+            f"{'Nome da biblioteca'.ljust(25)} | {'Nota média' .ljust(25)} | {'Status'}")
         for biblioteca in Biblioteca.bibliotecas:
-            print(f"{biblioteca.nome.ljust(25)} | {biblioteca.ativo}")
+            print(
+                f"{biblioteca.nome.ljust(25)} | {str(biblioteca.media_avaliacoes).ljust(25)} {biblioteca.ativo}")
 
     def alterna_estado(self):
         self._ativo = not self._ativo
@@ -21,3 +27,15 @@ class Biblioteca:
     @property
     def ativo(self):
         return "ativado" if self._ativo else "desativado"
+
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacoes(self):
+        if not self._avaliacao:
+            return '-'
+        soma = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        media = round(soma / len(self._avaliacao), 1)
+        return media
