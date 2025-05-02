5ad82dd93acc102a3d4eb8d29499dd7821ce1b51
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-05-02 10:03:58 -0300

    Utilizando Heranca

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -1,17 +1,24 @@
 from modelos.biblioteca import Biblioteca
+from modelos.itens.livro import Livro
+from modelos.itens.revista import Revista
 
 biblioteca_cidade = Biblioteca("Biblioteca da Cidade")
 biblioteca_shopping = Biblioteca("Biblioteca do Shopping")
 
-biblioteca_cidade.alterna_estado()
+livro1 = Livro("1984", "George Orwell", 30.0, "084-3245")
+revista1 = Revista("National Geographic", "John Doe", 15.0, "Quinta")
 
-biblioteca_cidade.receber_avaliacao('Fulano', 8.5)
-biblioteca_cidade.receber_avaliacao('Ciclano', 9.5)
-biblioteca_cidade.receber_avaliacao('Fulano', 7.0)
+# biblioteca_cidade.alterna_estado()
+
+# biblioteca_cidade.receber_avaliacao('Fulano', 8.5)
+# biblioteca_cidade.receber_avaliacao('Ciclano', 9.5)
+# biblioteca_cidade.receber_avaliacao('Fulano', 7.0)
 
 
 def main():
-    Biblioteca.listar_bibliotecas()
+    # Biblioteca.listar_bibliotecas()
+    print(vars(livro1))
+    print(vars(revista1))
 
 
 if __name__ == "__main__":
