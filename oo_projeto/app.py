036082d63b4bdbd4898d55323b016cb80748eda5
Adriano B Cerutti <adrianocerutti@gmail.com>
2025-05-02 10:32:26 -0300

    Metodo Abstrato

--- oo_projeto/app.py
+++ oo_projeto/app.py
@@ -9,6 +9,9 @@ livro1 = Livro("1984", "George Orwell", 30.0, "084-3245")
 livro2 = Livro("Brave New World", "Aldous Huxley", 25.0, "084-3245321")
 revista1 = Revista("National Geographic", "John Doe", 15.0, "Quinta")
 
+livro1.aplicar_desconto()
+revista1.aplicar_desconto()
+
 biblioteca_cidade.adicionar_item(livro1)
 biblioteca_cidade.adicionar_item(revista1)
 biblioteca_cidade.adicionar_item(livro2)
